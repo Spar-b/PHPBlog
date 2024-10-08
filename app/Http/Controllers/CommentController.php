a48191d42a52e5afd41a3b14701387f13da82b97
Sparb <spartanfastfun@gmail.com>
2024-10-08 22:02:44 +0300

    Minor changes to controllers

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request, Post $post)
+    {
+        $request->validate([
+            'body' => 'required',
+        ]);
+
+        Comment::create([
+            'body' => $request->body,
+            'post_id' => $post->id,
+            'user_id' => auth()->id(),
+        ]);
+
+        return back();
+    }
+
+    
 }
